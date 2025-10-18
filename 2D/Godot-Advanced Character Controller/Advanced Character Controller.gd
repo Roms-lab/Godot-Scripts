9a18be4d94fceae06b2d9e1cf3e11fa63872c5c5
Zach <100164907@ccisd.net>
2025-10-18 15:21:34 -0500

    Add files via upload

---
+++ 2D/Godot-Advanced Character Controller/Advanced Character Controller.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Script Started")
+
+@export var Speed = 100.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W):
+		position.y -= Speed * delta
+		rotation_degrees = -180
+	
+	if Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S):
+		position.y += Speed * delta
+		rotation_degrees = 0
+	if Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D):
+		position.x += Speed * delta
+		rotation_degrees = -90
+		
+	if Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_A):
+		position.x -= Speed * delta
+		rotation_degrees = 90
